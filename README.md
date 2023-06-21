7ca25cc5f1a9f16883a296532ae264b658b6e4d8
@ <dongshaobo@elifev.com.cn>
2023-06-21 16:31:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,107 @@
+
+# 仿QQ头像裁剪  
+ 
+
+   
+    
+
+![github](https://github.com/zhongruiAndroid/CropBitmap/blob/master/imagedirectory/clipbitmap2.gif "github")  
+
+## [Demo.apk下载](https://raw.githubusercontent.com/zhongruiAndroid/CropBitmap/master/app/sampledata/app.apk "apk文件")
+    
+
+| 属性           | 类型      | 说明                                                                  |
+|----------------|-----------|-----------------------------------------------------------------------|
+| maskColor      | color | 遮罩层颜色(默认#60000000)                                |
+| borderColor     | color | 裁剪框内部边框颜色(默认白色)                               |
+| radius        | dimension     | 裁剪框圆角(默认为最大值，裁剪框高度的一半)                                                        |
+| maxScale    | float     | 图片最大放大倍数(默认3)                                                      |
+| doubleClickScale    | float | 双击图片放大倍数(默认1.8,最大值不超过maxScale)
+
+  
+```xml
+<com.github.cropbitmap.LikeQQCropView
+    android:id="@+id/likeView"
+    android:layout_width="match_parent"
+    android:layout_height="0dp"
+    android:layout_weight="1"
+    android:background="@color/white"
+    />
+```
+
+
+#### 设置Bitmap
+```java
+LikeQQCropView likeView=findViewById(R.id.likeView);
+
+//如果传入的bitmap过大,此方法有OOM的可能
+likeView.setBitmap(Bitmap bitmap);
+
+//以下方法很安全,做了防止OOM的压缩
+
+/**设置压缩之后的宽和高*/
+likeView.setBitmap(多参);
+
+/**[强烈推荐该方法]设置压缩之后的宽度(高度自适应)*/
+likeView.setBitmapForWidth(param,屏幕宽度或者view宽度);
+
+/**[推荐该方法]设置压缩之后的高度(宽度自适应)*/
+likeView.setBitmapForHeight(多参);
+
+/**设置压缩的缩放倍数(偶数),图片缩小一半传2,缩小4倍传4*/
+likeView.setBitmapForScale(多参);
+
+```
+#### 对Bitmap的操作
+```java
+/**水平翻转*/
+likeView.horizontalFlip();
+
+/**垂直翻转*/
+likeView.verticalFlip();
+
+/**垂直+水平翻转*/
+likeView.verticalAndHorizontalFlip();
+
+/**裁剪*/
+likeView.clip();
+
+/**图片位置重置*/
+likeView.reset();
+
+/**设置遮罩层*/
+likeView.setMaskColor(color);
+
+/**设置裁剪框内部边框颜色*/
+likeView.setBorderColor(color);
+
+/**设置裁剪框圆角*/
+likeView.setRadius(radius);
+
+/**设置图片最大放大倍数*/
+likeView.setMaxScale(3);
+
+/**设置双击图片放大倍数*/
+likeView.setDoubleClickScale(1.8);
+
+/**获取裁剪框宽度*/
+likeView.getClipWidth();
+```
+<br/>
+
+### 如果本库对您有帮助,还希望支付宝扫一扫下面二维码,你我同时免费获取奖励金(非常感谢 Y(^-^)Y)
+![github](https://github.com/zhongruiAndroid/SomeImage/blob/master/image/small_ali.jpg?raw=true "github")  
+
+
+| 最新版本号 | [ ![Download](https://api.bintray.com/packages/zhongrui/mylibrary/CropBitmap/images/download.svg) ](https://bintray.com/zhongrui/mylibrary/CropBitmap/_latestVersion) |
+|--------|----|
+  
+
+
+
+```gradle
+implementation 'com.github:MyCropBitmap:版本号看上面'
+```  
+
+#### 历史版本说明
+- 1.0.4 解决图片缩放+翻转之后裁剪得到的bitmap宽高过大的问题
---
+++ app/src/main/java/com/test/cropview/MainActivity.java
@@ -0,0 +1,110 @@
+package com.test.cropview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import com.github.cropbitmap.LikeQQCropView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LikeQQCropView likeView;
+    SeekBar sb;
+    Button btHorizontalFlip;
+    Button btVerticalFlip;
+    Button btBoth;
+    Button btReset;
+    Button btClip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView() {
+        likeView=findViewById(R.id.likeView);
+        sb=findViewById(R.id.sb);
+
+        btHorizontalFlip=findViewById(R.id.btHorizontalFlip);
+        btHorizontalFlip.setOnClickListener(this);
+
+        btVerticalFlip=findViewById(R.id.btVerticalFlip);
+        btVerticalFlip.setOnClickListener(this);
+
+        btBoth=findViewById(R.id.btBoth);
+        btBoth.setOnClickListener(this);
+
+        btReset=findViewById(R.id.btReset);
+        btReset.setOnClickListener(this);
+
+        btClip=findViewById(R.id.btClip);
+        btClip.setOnClickListener(this);
+
+
+
+        //压缩bitmap宽度至1080
+//        Bitmap bitmap = LikeQQCropViewUtils.compressBitmapForWidth(this, R.drawable.bird, 1080);
+        //如果你通过储存路径从手机相册直接获取图片(未压缩)，在保证bitmap不会oom的情况下，可以直接调用setBitmap方法
+        //否则乖乖调用  以下方法(这些方法可以防止OOM)
+        //推荐使用setBitmapForHeight()
+        //推荐使用setBitmapForWidth()
+        //setBitmapForScale()
+        //setBitmap(多参)
+
+        likeView.setBitmapForWidth(R.drawable.bird,1080);
+
+
+
+
+
+
+
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //动态改变裁剪框圆角
+                float cropWidth = likeView.getClipWidth() / 2;
+                float newRadius = progress * 1f / sb.getMax() * cropWidth;
+                likeView.setRadius(newRadius);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btHorizontalFlip:
+                //水平翻转
+                likeView.horizontalFlip();
+            break;
+            case R.id.btVerticalFlip:
+                //垂直翻转
+                likeView.verticalFlip();
+            break;
+            case R.id.btBoth:
+                //垂直水平翻转
+                likeView.verticalAndHorizontalFlip();
+            break;
+            case R.id.btReset:
+                //重置图片位置
+                likeView.reset();
+            break;
+            case R.id.btClip:
+                //裁剪
+                TestBean.bitmap=likeView.clip();
+                Intent intent = new Intent(this, TestActivity.class);
+                startActivity(intent);
+            break;
+        }
+    }
+}
